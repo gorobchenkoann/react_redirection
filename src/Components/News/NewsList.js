0024fedf93484d4d9fa3995498acec16f2f34249
gorobchenkoann <gorobchenko.ann@gmail.com>
2018-10-05 00:30:31 +0300

    fix keys in news

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -9,9 +9,8 @@ export class NewsList extends React.Component {
                 {this.props.news.map((news) => 
                     <li key={news.id} className={styles.item}>
                         <h2 className={styles.title}>{news.title}</h2>
-                        {/* <p>{news.text}</p> */}
-                        {news.text.map((line) => 
-                            <p className={styles.text}>{line}</p>
+                        {news.text.map((line, index) => 
+                            <p key={index} className={styles.text}>{line}</p>
                         )}
                         <p className={styles.date}>{news.date}</p>
                     </li>
